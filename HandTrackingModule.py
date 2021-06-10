a00fa167ee5198e0df0e267946df6664c700c7f1
nrdnisml <nurudinismail69@gmail.com>
2021-06-10 22:24:44 +0700

    version 2 done

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -1,7 +1,7 @@
 import cv2
 import mediapipe as mp
 import time
-
+import math
 
 class handDetector():
     def __init__(self, mode=False, maxHands=2, detectionCon=0.7, trackCon=0.7):
@@ -14,6 +14,7 @@ class handDetector():
         self.hands = self.mpHands.Hands(self.mode, self.maxHands,
                                         self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
+        self.tipIds = [4, 8, 12, 16, 20]
 
     def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -28,20 +29,57 @@ class handDetector():
         return img
 
     def findPosition(self, img, handNo=0, draw=True):
-        lmList = []
+        xList = []
+        yList = []
+        bounding_box = []
+        self.lmList = []
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
             for id, lm in enumerate(myHand.landmark):
-                # print(id, lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                # print(id, cx, cy)
-                lmList.append([id, cx, cy])
+                xList.append(cx)
+                yList.append(cy)
+                self.lmList.append([id, cx, cy])
                 if draw:
                     cv2.circle(img, (cx, cy), 6, (255, 0, 0), cv2.FILLED)
+            xMin, xMax = min(xList), max(xList)
+            yMin, yMax = min(yList), max(yList)
+            bounding_box = xMin, yMin, xMax, yMax
+            if draw:
+                cv2.rectangle(img,(bounding_box[0]-20, bounding_box[1]-20),(bounding_box[2]+20, bounding_box[3]+20),(0,255,0), 2)
+        return self.lmList,bounding_box
 
-        return lmList
+    def fingersUp(self):
+        fingers = []
+        #jempol
+        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
+            fingers.append(1)
+        else :
+            fingers.append(0)
+        #4 Fingers
+        for id in range(1,5):
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id]-2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        return fingers
 
+    def findDistance(self,point1, point2, img, draw=True ):
+        # koordinat antara telunjuk dan jempol
+        x1, y1 = self.lmList[point1][1], self.lmList[point1][2]
+        x2, y2 = self.lmList[point2][1], self.lmList[point2][2]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+        if draw:
+            cv2.circle(img, (x1, y1), 10, (255, 0, 0), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 10, (255, 0, 0), cv2.FILLED)
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
+            cv2.circle(img, (cx, cy), 10, (255, 0, 0), cv2.FILLED)
+
+        # cari panjang line
+        # panjang line 50 - 400
+        lengthLine = math.hypot(x2 - x1, y2 - y1)
+        return lengthLine, img, [x1,y1,x2,y2,cx,cy]
 
 # def main():
 #     pTime = 0
--- VolumeControl.py
+++ VolumeControl.py
@@ -1,9 +1,7 @@
 import time
 import numpy as np
 import cv2
-import mediapipe as mp
 import HandTrackingModule as htm
-import math
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
@@ -41,51 +39,49 @@ def main():
     interface = devices.Activate(
         IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
     volume = cast(interface, POINTER(IAudioEndpointVolume))
-    # volume.GetMute()
-    # volume.GetMasterVolumeLevel()
-
-    volumeRange = volume.GetVolumeRange()
-    volumeMax = volumeRange[1]
-    volumeMin = volumeRange[0]
-    volBar = 500
+    volBar = 350
     volPercent = 0
-    myPCVolume = 0
-
+    colorVolume = (255,0,0)
     while True:
         success, img = vc.cap.read()
+        # deteksi tangan pada camera
         img = detector.findHands(img)
-        lmList = detector.findPosition(img, draw=False)
+        lmList,bounding_box = detector.findPosition(img, draw=True)
         if len(lmList) != 0 :
-            # print(lmList[4],lmList[8])
-            # koordinat
-            x1,y1 = lmList[4][1], lmList[4][2]
-            x2,y2 = lmList[8][1], lmList[8][2]
-            cx,cy = (x1+x2)//2, (y1+y2)//2
 
-            cv2.circle(img, (x1,y1), 10, (255,0,0), cv2.FILLED)
-            cv2.circle(img, (x2,y2), 10, (255,0,0), cv2.FILLED)
-            cv2.line(img, (x1,y1),(x2,y2), (255,0,0),3)
-            cv2.circle(img, (cx,cy), 10, (255,0,0), cv2.FILLED)
+            #Convert ukuran tangan ke bounding box
+            luas_bounding_box = (bounding_box[2]-bounding_box[0]) * (bounding_box[3]-bounding_box[1])//1000
 
-            #cari panjang line
-            # panjang line 50 - 400
-            lengthLine = math.hypot(x2-x1,y2-y1)
+            if 70<luas_bounding_box<300:
+                #Jarak antara jempol dan telunjuk
+                lengthLine, img, lengtInfo= detector.findDistance(4,8,img)
+                print(lengthLine)
 
-            # convert range line kedalam range volume
-            myPCVolume = np.interp(lengthLine, [50, 400], [volumeMin,volumeMax])
-            volBar = np.interp(lengthLine, [50,400], [500,150])
-            volPercent = np.interp(lengthLine, [50,400], [0,100])
-            print(lengthLine,myPCVolume)
-            # set volume
-            volume.SetMasterVolumeLevel(myPCVolume, None)
+                #convert volume
+                volBar = np.interp(lengthLine, [50, 350], [350, 150])
+                volPercent = np.interp(lengthLine, [50, 350], [0, 100])
 
-            if lengthLine<=50:
-                cv2.circle(img, (cx,cy), 10, (0,0,255), cv2.FILLED)
+
+                #Kurangi resolusi agar lebih smooth
+                smoothness = 5
+                volPercent = smoothness * round(volPercent/smoothness)
+
+                #Cek jari lain up untuk set volume
+                fingers = detector.fingersUp()
+                if fingers[4] :
+                    # set volume
+                    colorVolume = (0, 0, 255)
+                    volume.SetMasterVolumeLevelScalar(volPercent / 100, None)
+                    cv2.circle(img, (lengtInfo[4], lengtInfo[5]), 10, (0, 0, 255), cv2.FILLED)
+                else :
+                    colorVolume = (255, 0, 0)
 
         # gambar volume bar
-        cv2.rectangle(img, (50,150), (85,400), (255,0,0), 3)
-        cv2.rectangle(img, (50,int(volBar)), (85,400), (255,0,0), cv2.FILLED)
-        cv2.putText(img, f'{int(volPercent)} %', (40,500), cv2.FONT_HERSHEY_COMPLEX, 1, (0 ,255,0), 3)
+        cv2.rectangle(img, (50,150), (85,350), (255,0,0), 3)
+        cv2.rectangle(img, (50,int(volBar)), (85,350), (255,0,0), cv2.FILLED)
+        cv2.putText(img, f'{int(volPercent)} %', (40,400), cv2.FONT_HERSHEY_COMPLEX, 1, (0,0,255), 3)
+        currentVolume = volume.GetMasterVolumeLevelScalar()*100
+        cv2.putText(img, f'Vol Set: {int(currentVolume)}', (600,70), cv2.FONT_HERSHEY_COMPLEX, 1, colorVolume, 3)
 
         vc.showFps(img)
         cv2.imshow("Gambar", img)
